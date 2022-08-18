86a7c1fa47b4df2b1e733fe31ffcbf6974f99fad
wipzhu <wipzhu@qq.com>
2022-08-18 21:19:58 +0800

    test demo

--- src/com/wipzhu/test/Test2.java
+++ src/com/wipzhu/test/Test2.java
@@ -5,8 +5,10 @@ public class Test2 {
         // 输出101-200之间的所有质数，并输出个数
 
         int count = 0;
+        //遍历101-200
         for (int i = 101; i <= 200; i++) {
             boolean flag = true;
+            //判断当前数是否是一个质数
             for (int j = 2; j < i; j++) {
                 if (i % j == 0) {
                     flag = false;
