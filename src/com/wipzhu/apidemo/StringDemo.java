0a86c1e636c9ae103b658238be708050f330154c
weipeng.zhu <weipeng.zhu@yintech.cn>
2022-08-31 09:42:20 +0800

    字符串联系

--- src/com/wipzhu/apidemo/StringDemo.java
+++ src/com/wipzhu/apidemo/StringDemo.java
@@ -1,5 +1,7 @@
 package com.wipzhu.apidemo;
 
+import java.util.Scanner;
+
 public class StringDemo {
 
     public static void main(String[] args) {
@@ -21,10 +23,18 @@ public class StringDemo {
 //        String s3 = new String("Abc");
 //
 ////        System.out.println(s1 == s3); // false
+////        System.out.println(s1 == s2); // true
 //        System.out.println(s1.equals(s3));
 //        System.out.println(s1.equalsIgnoreCase(s3));
 
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("请输入abc");
+        String str1 = sc.next();
+        String str2 = "abc";
+
+//        System.out.println(s1 == s2); // false
+        System.out.println(str1.equals(str2));
 
     }
 
