b703c2b35637445760af0ed5f19a013d0a0ece48
wipzhu <wipzhu@qq.com>
2022-09-01 21:47:39 +0800

    字符串练习

---
+++ src/com/wipzhu/apidemo/StringDemo8.java
@@ -0,0 +1,15 @@
+package com.wipzhu.apidemo;
+
+public class StringDemo8 {
+    public static void main(String[] args) {
+        //StringBuilder使用
+        String s1 = "aaa";
+        String s2 = "bbb";
+        String s3 = "ccc";
+        String s4 = "ddd";
+        String s5 = "eee";
+
+        StringBuilder str1 = new StringBuilder();
+
+    }
+}
