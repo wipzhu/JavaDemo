b74bd6d2802cf49f7f1ddce9b5ebffb7d04a204b
wipzhu <wipzhu@qq.com>
2022-09-16 22:34:46 +0800

    ArrayList

--- src/com/wipzhu/apidemo/ArrayListDemo4.java
+++ src/com/wipzhu/apidemo/ArrayListDemo4.java
@@ -3,7 +3,7 @@ package com.wipzhu.apidemo;
 import java.util.ArrayList;
 
 public class ArrayListDemo4 {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         ArrayList<User> list = new ArrayList<>();
 
         User u1 = new User("heima001", "zhangsan", "123456");
@@ -18,14 +18,29 @@ public class ArrayListDemo4 {
         System.out.println(flag);
     }
 
+    /**
+     * 判断集合中是否包含某个id的元素
+     * @param list
+     * @param id
+     * @return
+     */
     public static boolean contains(ArrayList<User> list, String id) {
+        return getIndex(list, id) >= 0;
+    }
+
+    /**
+     * 集合中若包含某个id的元素则返回索引，否则返回-1
+     * @param list
+     * @param id
+     * @return
+     */
+    public static int getIndex(ArrayList<User> list, String id) {
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).getId().equals(id)) {
-                return true;
+                return i;
             }
         }
 
-        return false;
+        return -1;
     }
-
 }
